edcd6d2e71a387c791febc162e8aa0f3fa25a804
joe-code-net <joe.code.netgmail.com>
2019-04-23 22:32:01 -0600

    functions to add clients

--- sales-app/main.py
+++ sales-app/main.py
@@ -1,7 +1,29 @@
 #Clients List
-clients = 'pablo,ricardo,'
+clients = 'Pablo, Ricardo, '
 
-#Main Function
+#Function to create a new client
+def create_client(client_name):
+    global clients #global for use the clients variable with the global values
+	
+    clients += client_name
+    _add_coma()
+
+#Function to add a comma et the end of customers list
+def _add_coma(): 
+   global clients #global for use the clients variable with the global values
+   clients += ', '
+
+#Function to print the list of clients
+def list_clients(): 
+	global clients
+	print (clients)
+
+#Main function
 if __name__ == '__main__':
-    clients += 'david'
-    print(clients)
+	
+	create_client('Joe') #ejecutamos la funcion crear cliente
+	
+	list_clients() #Volvemos a ejecutar la funcion listar clientes 
+
+#To wait the user action
+input()

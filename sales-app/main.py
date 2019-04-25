bd69d2980ffc455ca0f7c217d194479475fa652e
joe-code-net <joe.code.netgmail.com>
2019-04-24 21:23:06 -0600

    conditionals

--- sales-app/main.py
+++ sales-app/main.py
@@ -3,10 +3,13 @@ clients = 'Pablo, Ricardo, '
 
 #Function to create a new client
 def create_client(client_name):
-    global clients #global for use the clients variable with the global values
+   global clients #global for use the clients variable with the global values
 	
-    clients += client_name
-    _add_coma()
+   if client_name not in clients:
+      clients += client_name
+      _add_coma()
+   else:
+      print('Client alredy is in the client\'s list')
 
 #Function to add a comma et the end of customers list
 def _add_coma(): 
@@ -18,12 +21,27 @@ def list_clients():
 	global clients
 	print (clients)
 
+def _print_welcome():
+   print('WELCOME TO SALES PLATZI APP')
+   print('*' * 50)
+   print('What would you like to do today?')
+   print('[C]reate client')
+   print('[D]elete client')
+
 #Main function
 if __name__ == '__main__':
-	
-	create_client('Joe') #ejecutamos la funcion crear cliente
-	
-	list_clients() #Volvemos a ejecutar la funcion listar clientes 
+   _print_welcome() 
+    
+   command = input()
+    
+   if command == 'C':
+      client_name = input('What is the client name? ')
+      create_client(client_name)
+      list_clients()
+   elif command == 'D':
+      pass
+   else:
+      print('Invalid command') 
 
 #To wait the user action
 input()

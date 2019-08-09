6942d55dfadefd044118041cf3467f641f3fcda6
joe-code-net <joe.code.netgmail.com>
2019-08-08 22:19:58 -0600

    Listas en el proyecto

--- sales-app/main.py
+++ sales-app/main.py
@@ -1,15 +1,15 @@
 import sys
 
 #Clients List
-clients = 'Pablo, Ricardo, '
+clients = ['Pablo', 'Ricardo']
 
 #Function to create a new client
 def create_client(client_name):
    global clients #global for use the clients variable with the global values
 	
    if client_name not in clients:
-      clients += client_name
-      _add_coma()
+      #clients += client_name
+      clients.append(client_name)
    else:
       print('Client alredy is in the client\'s list')
 
@@ -18,36 +18,33 @@ def delete_client(client_name):
    global clients #global for use the clients variable with the global values
 
    if client_name in clients:
-      clients = clients.replace(client_name + ',', '')
+      #clients = clients.replace(client_name + ',', '')
+      clients.remove(client_name)
    else:
       print('Client is not in clients list')
 
 #Function to search a client
 def search_client(client_name):
-   clients_list = clients.split(',')
+   #clients_list = clients.split(',')
 
-   for client in clients_list:
+   for client in clients:
       if client != client_name:
           continue
       else:
          return True
 
-
-#Function to add a comma et the end of customers list
-def _add_coma(): 
-   global clients #global for use the clients variable with the global values
-   clients += ', '
-
 #Function to print the list of clients
-def list_clients(): 
-	global clients
-	print (clients)
+def list_clients():
+   for idx, client in enumerate(clients):
+	      print ('{} : {}'.format(idx, client))
 
 def update_client(client_name, update_client_name):
    global clients
 
    if client_name in clients:
-          clients = clients.replace(client_name + ',', update_client_name + ',')
+          #clients = clients.replace(client_name + ',', update_client_name + ',')
+          index = clients.index(client_name)
+          clients[index] = update_client_name
    else:
           print('Client is not in clients list')
 
@@ -55,7 +52,8 @@ def _print_welcome():
    print('WELCOME TO SALES PLATZI APP')
    print('*' * 50)
    print('What would you like to do today?')
-   print('[C]reate client')
+   print('[C]reate client') 
+   print('[L]ist clients')
    print('[U]date client')
    print('[D]elete client')
    print('[S]earch client')
@@ -86,6 +84,8 @@ if __name__ == '__main__':
    if command == 'C':
       client_name = _get_client_name()
       create_client(client_name)
+      list_clients()
+   elif command == 'L':
       list_clients()
    elif command == 'D':
       client_name = _get_client_name()

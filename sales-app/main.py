adcd28ee9fd83f262f64090931b125225ac41847
joe-code-net <joe.code.netgmail.com>
2019-09-10 21:51:06 -0600

    Archivos

--- sales-app/main.py
+++ sales-app/main.py
@@ -1,20 +1,27 @@
-import sys
+import csv
+import os
 
-#Clients List
-clients = [
-   {
-      'name': 'Pablo',
-      'company': 'Google',
-      'email': 'pablo@google.com',
-      'position': 'Software engineer',
-   },
-   {
-      'name': 'Ricardo',
-      'company': 'Facebook',
-      'email': 'ricardo@facebook.com',
-      'position': 'Data engineer',
-   }
-]
+CLIENT_TABLE = '.clients.csv'
+CLIENT_SCHEMA = ['name', 'company', 'email', 'position']
+clients =[]
+
+def _initialize_clients_from_storage():
+	with open(CLIENT_TABLE, mode ='r') as f:
+		reader = csv.DictReader(f, fieldnames= CLIENT_SCHEMA)
+
+		for row in reader: 
+			clients.append(row)
+
+def _save_clients_to_storage():
+	tmp_table_name = '{}.tmp'.format(CLIENT_TABLE)
+	with open(tmp_table_name, mode='w') as f:
+		writer = csv.DictWriter(f, fieldnames=CLIENT_SCHEMA)
+		writer.writerows(clients)
+		
+
+		os.remove(CLIENT_TABLE)
+		f.close
+		os.rename(tmp_table_name, CLIENT_TABLE) 
 
 #Function to create a new client
 def create_client(client):
@@ -91,6 +98,8 @@ def _print_welcome():
 
 #Main function
 if __name__ == '__main__':
+   _initialize_clients_from_storage()
+   
    _print_welcome() 
     
    command = input()
@@ -99,20 +108,20 @@ if __name__ == '__main__':
    if command == 'C':
       client = _get_client_from_user()
       create_client(client)
-      list_clients()
+      #list_clients()
    elif command == 'L':
       list_clients()
    elif command == 'D':
       client_id = int(_get_client_field('id'))
 
       delete_client(client_id)
-      list_clients()
+      #list_clients()
    elif command == 'U':
       client_id = int(_get_client_field('id'))
       updated_client = _get_client_from_user()
 
       update_client(client_id, updated_client)
-      list_clients()
+      #list_clients()
    elif command == 'S':
       client_name = _get_client_field('name')
       found = search_client(client_name)
@@ -124,5 +133,7 @@ if __name__ == '__main__':
    else:
       print('Invalid command') 
 
+   _save_clients_to_storage()
+
 #To wait the user action
-input()
+#input()

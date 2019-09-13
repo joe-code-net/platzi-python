5952d1e06f1366c5f5a58f7d5b780cca4eb2e479
joe-code-net <joe.code.netgmail.com>
2019-09-12 21:44:42 -0600

    Decoradores

---
+++ decorators/decorators.py
@@ -0,0 +1,32 @@
+PASSWORD = '12345'
+
+def password_required(func):
+    def wrapper():
+        password = input('Cual es tu contrasena? ')
+
+        if password == PASSWORD:
+            return func()
+        else:
+            print('La contrasena no es correcta')
+    
+    return wrapper
+
+@password_required
+def need_password():
+    print('La contraseña es correcta')
+
+def upper(func):
+    def wrapper(*args, **kwargs):
+        result = func(*args, **kwargs)
+
+        return result.upper()
+
+    return wrapper
+
+@upper
+def say_my_name(name):
+    return 'Hola, {}'.format(name)
+
+
+if __name__ == '__main__':
+    print(say_my_name('David'))
